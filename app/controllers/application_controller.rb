30d3027961c96a3b4f09f8276e62d3fab5338548
wonder-boooy <yuma.wada.1127@gmail.com>
2019-01-27 20:41:28 +0900

    test
    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'BASIC_AUTH_USER' && password == 'BASIC_AUTH_PASSWORD'
+      username == 'admin' && password == '2222'
     end
   end
 end
