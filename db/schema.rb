9c6b76369098868fb39498382455ca4d71f11529
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-02 19:37:47 +0900

    create category-model & table
    categoryモデルとcategoriesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129025014) do
+ActiveRecord::Schema.define(version: 20190202103138) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "parent_id"
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                      null: false
