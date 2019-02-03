c972c1b3c80fc5493ebddc8c87938041e39f8e0e
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-03 15:41:19 +0900

    create category-table
    categoryテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190203061200) do
+ActiveRecord::Schema.define(version: 20190203063311) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "list",       null: false
