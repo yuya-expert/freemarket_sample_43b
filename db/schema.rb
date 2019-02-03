1c413f5e2b0af634ac5c29789b382a90035f7bf3
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-03 17:17:37 +0900

    create like-model
    likeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190203072406) do
+ActiveRecord::Schema.define(version: 20190203081129) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "list",       null: false
@@ -25,6 +25,35 @@ ActiveRecord::Schema.define(version: 20190203072406) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "delivery_addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "postal_code"
+    t.string   "prefecture",  null: false
+    t.string   "city",        null: false
+    t.string   "address",     null: false
+    t.string   "building"
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_delivery_addresses_on_user_id", using: :btree
+  end
+
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "image",      null: false
+    t.integer  "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id", using: :btree
+  end
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_likes_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
+  end
+
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                      null: false
     t.text     "detail",          limit: 65535,             null: false
@@ -36,7 +65,21 @@ ActiveRecord::Schema.define(version: 20190203072406) do
     t.integer  "delivery_status",               default: 1, null: false
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
-    t.integer  "category"
+    t.integer  "user_id"
+    t.integer  "brand_id"
+    t.integer  "category_id"
+    t.index ["brand_id"], name: "index_products_on_brand_id", using: :btree
+    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_products_on_user_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "rate",                     null: false
+    t.text     "comment",    limit: 65535
+    t.integer  "user_id",                  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -65,4 +108,7 @@ ActiveRecord::Schema.define(version: 20190203072406) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "delivery_addresses", "users"
+  add_foreign_key "images", "products"
+  add_foreign_key "reviews", "users"
 end
