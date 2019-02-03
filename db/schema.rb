859a10b0f70b0e99feec2bba5de12c5baf62126f
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-03 15:14:17 +0900

    create brand-model
    brandモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129025014) do
+ActiveRecord::Schema.define(version: 20190203061200) do
+
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "list",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "parent_id"
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                      null: false
@@ -20,9 +33,10 @@ ActiveRecord::Schema.define(version: 20190129025014) do
     t.integer  "area",                                      null: false
     t.integer  "shipping_dates",                            null: false
     t.integer  "price",                                     null: false
-    t.integer  "delivery_status",               default: 0, null: false
+    t.integer  "delivery_status",               default: 1, null: false
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
+    t.integer  "category"
   end
 
 end
