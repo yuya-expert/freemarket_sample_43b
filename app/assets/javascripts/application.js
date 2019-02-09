1b3975c528b198710fa533becc6ceb71342e5346
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-09 16:53:49 +0900

    create validation js
    バリデーションのjs作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,48 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  // ユーザー新規登録時のバリデーション
+  $("#new_user").submit(function(e){
+    var judge = 0;
+    // メールアドレス判定
+    if($(".sign-up__email").val().length == 0) {
+      $(".email-error").show();
+      $(".email-format-error").hide();
+      judge += 1;
+    } else if($(".sign-up__email").val().match(/.+@.+/)) {
+      $(".email-error").hide();
+      $(".email-format-error").hide();
+    } else {
+      $(".email-error").hide();
+      $(".email-format-error").show();
+      judge += 1;
+    }
+    // パスワード判定
+    if($(".sign-up__password").val().length == 0) {
+      $(".password-error").show();
+      $(".password-length-error").show();
+      judge += 1;
+    } else if($(".sign-up__password").val().length < 6  ) {
+      $(".password-error").hide();
+      $(".password-error-length").show();
+      judge += 1;
+    } else {
+      $(".password-error").hide();
+      $(".password-length-error").hide();
+    }
+    // パスワード(確認)判定
+    if($(".sign-up__password_confirmation").val().length == 0) {
+      $(".password_confirmation-error").show();
+      judge += 1;
+    } else {
+      $(".password_confirmation-error").hide();
+    }
+    if(judge != 0) {
+      console.log(judge);
+      e.preventDefault();
+    }
+    console.log("ストップ");
+  })
+})
