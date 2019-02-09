3609463e915f49f726ac75d7323ffda34b42dc70
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-09 16:49:13 +0900

    update users-controller
    usersコントローラの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:main_data, :sms_confirmation]
+
+  def main_data
+  end
+
+  def sms_confirmation
+  end
+
+  def create
+    user = User.new(users_params)
+    if user.save
+      redirect_to controller: :products, action: :index
+    else
+      render :main_data
+    end
+  end
+
   def edit
   end
 
   def payments
+  end
+
+  def set_user
+    @user = User.new
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:email, :password)
   end
 end
