357a8b81b07cf18a5653e1196d33fda066133988
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-09 16:48:38 +0900

    set routes
    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:edit] do
+  resources :users, only: [:edit, :create] do
     member do
       get "payments"
+    end
+    collection do
+      get "main_data"
+      post "sms_confirmation"
     end
   end
 
