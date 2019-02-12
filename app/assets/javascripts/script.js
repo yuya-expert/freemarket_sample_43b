e354bc317fc75408b01e11f2b5b80fe1b0875927
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-12 12:54:00 +0900

    sum js
    jsファイルをまとめた

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -129,4 +129,17 @@ $(document).on("turbolinks:load",function(){
     results.empty();
   })
 })
-
+$(document).on("turbolinks:load",function(){
+  $("#price-field").keyup(function(){
+    if($(this).val() == "") {
+      $("#tax-field").html("-");
+      $("#profit-field").html("-");
+    }
+    var $price = $(this).val();
+    var tax = Math.floor($price * 0.1);
+    var profit = $price - tax;
+    var numberWithComma = new Intl.NumberFormat();
+    $("#tax-field").html("¥" + numberWithComma.format(tax));
+    $("#profit-field").html("¥" + numberWithComma.format(profit));
+  })
+})
