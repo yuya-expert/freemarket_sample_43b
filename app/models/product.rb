d1e794cdf70967029bd2dd4b397eb1a61de23c37
yuta-2v <yut.a.rumin@gmail.com>
2019-02-13 20:52:45 +0900

    Convert assosiation product to image

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :category
 
+  accepts_nested_attributes_for :images
+
   enum status:{'---': 0, '新品,未使用': 1, '未使用に近い': 2, '目立った傷や汚れなし': 3, 'やや傷や汚れあり': 4, '傷や汚れあり': 5, '全体的に状態が悪い': 6}, _suffix: true
   enum delivery_fee:{'---': 0, '送料込み(出品者負担)': 1, '着払い(購入者負担)': 2}
   enum shipping_dates:{'---': 0, '1~2日': 1, '2~3日': 2, '4~7日': 3}, _suffix: true
