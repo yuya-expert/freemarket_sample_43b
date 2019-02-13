2401e67341149bb0cd074cff4a15b3024fc5a151
yuta-2v <yut.a.rumin@gmail.com>
2019-02-13 20:53:13 +0900

    add products#edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-
+  protect_from_forgery except: :update
 
   def index
     @products = Product.limit(4).order("created_at desc")
@@ -30,6 +30,21 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    @images = Image.where(product_id: params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(images_params)
+      unless params[:images].nil?
+        params[:images]['image'].each do |i|
+          @image = @product.images.create!(image: i, product_id: @product.id)
+        end
+      end
+      redirect_to controller: :mypages , action: :index
+    else
+      redirect_to action: :edit
+    end
   end
 
   def destroy
@@ -41,22 +56,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  def detail
-    @product = Product.find(params[:id])
-    @product_user = Product.find_by(user_id: params[:user_id])
-    @category_id = Category.find(@product.category_id)
-    @category_child_id = Category.find(@category_id.parent_id)
-    @category_parent_id = Category.find(@category_child_id.parent_id)
-    @image = Image.find(params[:id])
-    @review_good = Review.where(rate: 0).count
-    @review_nomal = Review.where(rate: 1).count
-    @review_bad = Review.where(rate: 2).count
-    @user = User.find(params[:id])
-    @like = Like.new
-    @likes_count = Like.where(product_id: @product.id).count
-    @after_item = Product.order("RAND()").first
-    @before_item = Product.order("RAND()").last
-  end
 
   def confirmation
   end
@@ -70,4 +69,7 @@ class ProductsController < ApplicationController
     params.require(:image).permit(image: []).merge(product_id: @product.id)
   end
 
+  def images_params
+    params.require(:product).permit(:name, :detail, :status, :delivery_fee, :area, :shipping_dates, :price, :delivery_status,images_attributes: [:id, :image, :product_id])
+  end
 end
