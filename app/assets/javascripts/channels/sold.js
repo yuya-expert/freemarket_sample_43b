c03db2ef84af5f57b7753e116b12bdc283b189a5
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-14 21:36:42 +0900

    update sold.js
    sold out アイコンの表示条件更新

--- app/assets/javascripts/channels/sold.js
+++ app/assets/javascripts/channels/sold.js
@@ -1,24 +1,20 @@
-
-$(function(){
-var sold = $('.sold').val();
 $(document).on("turbolinks:load", function() {
+  var sold = $('.sold').val();
   $('.content-item-product-state-buy').hide();
   $('.message-submit.btn-gray-buy').hide();
   // 購入できる場合の処理
- if( sold == 0){
-    // 商品のSOLD OUT表示を削除
-  $('.item-tag-sold-out-top').hide();
-    // 商品購入ボタンの表示
-  $('.content-item-product-state-buy').show();
-    // 売り切れのためコメントできませんの非表示
-  $('.message-submit.btn-gray').hide();
-    // コメントするの表示
-  $('.message-submit.btn-gray-buy').show();
-  $('.buy-button-soldout').hide();
-  $('.buy-button-buy').show();
-  $('.content-item-product-state').hide();
-  $('.content-item-product-state-buy').show();
-  console.log("ffffff")
+  if( sold == "未定"){
+      // 商品のSOLD OUT表示を削除
+    $('.item-tag-sold-out-top').hide();
+      // 商品購入ボタンの表示
+    $('.content-item-product-state-buy').show();
+      // 売り切れのためコメントできませんの非表示
+    $('.message-submit.btn-gray').hide();
+      // コメントするの表示
+    $('.message-submit.btn-gray-buy').show();
+    $('.buy-button-soldout').hide();
+    $('.buy-button-buy').show();
+    $('.content-item-product-state').hide();
+    $('.content-item-product-state-buy').show();
   }
-  });
 });
