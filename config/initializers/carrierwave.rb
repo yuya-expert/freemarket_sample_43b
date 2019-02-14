69ffa703fd1faaba09ab042e77432aed1a816b87
wonder-boooy <yuma.wada.1127@gmail.com>
2019-02-14 12:54:48 +0900

    update carrierwave.rb
    carrierwave.rbの間違い修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'wonder-boooy-freemarket43b'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/wonder-boooy-freemarket43b'
+  config.fog_directory  = 'freemarket43b'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket43b'
 end
