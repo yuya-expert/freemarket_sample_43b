a259f5e20a0ef0ee6f38898d48aa3e20682b061c
luglio22 <sweet.c.july22@gmail.com>
2019-02-19 15:13:09 +0900

    ユーザー登録　コントローラーとルーティング
    what
    ユーザー登録機能
    why
    商品出品や売却の際の利便性向上

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:main_data, :sms_confirmation]
-
+  require "date"
   def main_data
+    @user = User.new
   end
 
   def sms_confirmation
@@ -17,8 +18,39 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  private
-  def users_params
-    params.require(:user).permit(:email, :password)
+  def certification
+    @user = User.new
+    year =params[:user]['birthday(1i)'].to_s
+    month =params[:user]['birthday(2i)'].to_s
+    day = params[:user]['birthday(3i)'].to_s
+    birthday = year + month + day
+    session[:nickname] = params[:user][:nickname]
+    session[:email] = params[:user][:email]
+    session[:password] = params[:user][:password]
+    session[:last_name] = params[:user][:last_name]
+    session[:first_name] = params[:user][:first_name]
+    session[:last_name_ja] = params[:user][:last_name_ja]
+    session[:first_name_ja] = params[:user][:first_name_ja]
+    session[:birthday] = birthday
+  end
+
+  def check
+    @user = User.new
+    session[:phone_number] = params[:user][:phone_number]
+  end
+
+  def address
+    @user = User.new
+  end
+
+  def creditcard
+    session[:postal_code] = params[:user][:postal_code]
+    session[:prefecture] = params[:user][:prefecture]
+    session[:city] = params[:user][:city]
+    session[:add_ress] = params[:user][:add_ress]
+    session[:building] = params[:user][:building]
+    session[:phone_number] = params[:user][:phone_number]
+
+    @user = User.new(nickname: session[:nickname],email: session[:email],password: session[:password],last_name: session[:last_name],first_name: session[:first_name],last_name_ja: session[:last_name_ja],first_name_ja: session[:first_name_ja],birthday: session[:birthday],postal_code: session[:position],prefecture: session[:prefecture],city: session[:city],add_ress: session[:add_ress],building: session[:building],phone_number: session[:phone_number])
   end
 end
