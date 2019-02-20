55d7c1084e94e835e3a0eb358e43bc2636fcfda0
yuya-expert <yuya@yuuyanoMacBook-puro.local>
2019-02-20 13:12:59 +0900

    コンフリクト の解消

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
 class Category < ApplicationRecord
   has_many :products
+
+  def middle_category
+    @large_category = Category.where(parent_id: [0])
+    @large_category.each do |lc|
+      @middle_category = Category.where(parent_id: lc.id)
+    end
+  end
 end
