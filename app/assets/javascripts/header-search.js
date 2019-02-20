72e0da2653f8e91b3c8971e24f89cd86c47e1b46
luglio22 <sweet.c.july22@gmail.com>
2019-02-20 17:07:21 +0900

    トップ画面なおし
    なおし

---
+++ app/assets/javascripts/header-search.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.input-default').click(function(){
+      $('.input-default').css("border","1px solid #0099e8");
+  })
+  $('.input-default').mouseout(function(){
+      $('.input-default').css("border","1px solid #ccc");
+  })
+});
